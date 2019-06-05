7d3672b66a59548dc606af9e2daac79de22a25ce
Joe Ganobsik <jganobsik@gmail.com>
2019-06-05 14:45:39 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate_user
-
+client_id = 
   def create
     response = Faraday.post "https://github.com/login/oauth/access_token" do |req|
   req.body = { 'client_id': client_id, 'client_secret': client_secret, 'code': code }
   req.headers['Accept'] = 'application/json'
 
-body = JSON.parse(response.body)
+  body = JSON.parse(response.body)
 end
   end
 end
